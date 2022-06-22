25d71f5224d66109de7f4d4f6e97bf39ae982eb4
Mansour Tarek Mansour <mansourtarek100@gmail.com>
2022-06-22 17:47:50 +0200

    products screen banner

--- lib/presentation/products/bloc/products_cubit.dart
+++ lib/presentation/products/bloc/products_cubit.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 import 'dart:developer';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:man_shop_app/data/models/home/home_model.dart';
-import 'package:man_shop_app/presentation/products/bloc/states.dart';
+import 'package:man_shop_app/presentation/products/bloc/products_states.dart';
 import 'package:man_shop_app/shared/components/constants.dart';
 import 'package:man_shop_app/shared/network/end_points.dart';
 import 'package:man_shop_app/shared/network/remote/dio_helper.dart';
